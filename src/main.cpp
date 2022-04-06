2084924dc151c70f12b63a61584959ec25b066f4
brucechanjianle <jianle001@e.ntu.edu.sg>
2022-04-06 14:20:07 +0800

    update cmake and source file. Can work as expected.

--- src/main.cpp
+++ src/main.cpp
@@ -5,19 +5,19 @@
 
 // ROS1
 #include "ros/ros.h"
-#include "sensor_msgs/PointCloud2.h"
+// #include "sensor_msgs/PointCloud2.h"
 
 // STL
 #include <chrono>
 #include <tuple>
 #include <string>
 
-// enum int 
+enum index {ros1_topic = 0, ros2_topic, ros1_type, ros2_type};
 
 int main(int argc, char ** argv)
 {
   /* initialize ROS 1 */
-  ros::init(argc, argv, "ros2_ros1_bridge");
+  ros::init(argc, argv, "ros2_ros2_bridge");
   ros::NodeHandle ros1_nh{};
 
 
@@ -25,11 +25,11 @@ int main(int argc, char ** argv)
   rclcpp::init(argc, argv);
   auto ros2_node = rclcpp::Node::make_shared("ros2_ros1_bridge");
 
-  std::tuple<std::string, std::string, std::string, std::string> ros2_to_ros1_pair = {"/vision60_points", "/mcu/command/pointcloud", "sensor_msgs/PointCloud2", "sensor_msgs/msg/PointCloud2"};
+  std::tuple<std::string, std::string, std::string, std::string> ros2_to_ros1_pair = {"/vision60_points", "/mcu/state/pointcloud", "sensor_msgs/PointCloud2", "sensor_msgs/msg/PointCloud2"};
 
   constexpr std::size_t queue_size = 10;
 
-  ros1_bridge::Bridge2to1Handles handle = ros1_bridge::create_bridge_from_2_to_1(ros2_node, ros1_nh, ros2type, topic, queue_size, ros1type, topic, queue_size);
+  ros1_bridge::Bridge2to1Handles handle = ros1_bridge::create_bridge_from_2_to_1(ros2_node, ros1_nh, std::get<index::ros2_type>(ros2_to_ros1_pair), std::get<index::ros2_topic>(ros2_to_ros1_pair), queue_size, std::get<index::ros1_type>(ros2_to_ros1_pair), std::get<index::ros1_topic>(ros2_to_ros1_pair), queue_size);
 
   /* ROS 1 async spinner */
   ros::AsyncSpinner async_spinner{1};
